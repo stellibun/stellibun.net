68f88c9c304a8b421e66b27132d1ee350a0f3e7f
viskyla <novapeache@gmail.com>
2026-01-26 00:30:55 -0500

    forgot to do main.css lol

--- assets/scripts/load.js
+++ assets/scripts/load.js
@@ -4,8 +4,8 @@ document.title = "viskyla.net";
 document.characterSet = "UTF-8";
 
 // Stylesheets
-//let mainCSS = createElement("link"); mainCSS.rel = "stylesheet"; mainCSS.href = "/assets/css/main.css";
+let mainCSS = createElement("link"); mainCSS.rel = "stylesheet"; mainCSS.href = "/assets/css/main.css";
 let silkCSS = createElement("link"); silkCSS.rel = "stylesheet"; silkCSS.href = "/assets/css/silk.css";
 
-//document.head.appendChild(mainCSS);
+document.head.appendChild(mainCSS);
 document.head.appendChild(silkCSS);
