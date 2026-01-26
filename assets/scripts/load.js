edd85446cf31ef26618c287dff4b0100c4937120
viskyla <novapeache@gmail.com>
2026-01-26 00:34:23 -0500

    Possible fix?

--- assets/scripts/load.js
+++ assets/scripts/load.js
@@ -2,8 +2,8 @@ document.title = "viskyla.net";
 document.characterSet = "UTF-8";
 
 // Stylesheets
-let mainCSS = createElement("link"); mainCSS.rel = "stylesheet"; mainCSS.href = "/assets/css/main.css";
-let silkCSS = createElement("link"); silkCSS.rel = "stylesheet"; silkCSS.href = "/assets/css/silk.css";
+let mainCSS = document.createElement("link"); mainCSS.rel = "stylesheet"; mainCSS.href = "/assets/css/main.css";
+let silkCSS = document.createElement("link"); silkCSS.rel = "stylesheet"; silkCSS.href = "/assets/css/silk.css";
 
 document.head.appendChild(mainCSS);
 document.head.appendChild(silkCSS);
